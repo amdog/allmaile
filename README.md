37444b410c140e688224775ccaeb71c0f96fc0f3
amdog <amdog@localhost.localdomain>
2020-12-18 22:24:17 +0800

    基本的前台

--- README.md
+++ README.md
@@ -1,27 +1,10 @@
-# Allmaile
+# allmaile 后台邮件管理系统
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.2.
+<kbd>angular --version = 11</kbd> 
 
-## Development server
+## 功能概要
+- 邮件的回复
+- 邮件删除
+- 按条件筛选邮件
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+## thank your man!
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'allmaile';
+  public messageType:string='bug'
+  public yearList:number[]=[]
+  public dayList:number[]=[]
+  public monthList:number[]=[]
+  public intervalList:any[]=[2020,1,1,2020,12,31]
+  public messageList:any[]=[]
+  public keyWords:string=''
+  public selectAll:boolean=false
+  public messageClickIndex:number=-1
+  constructor(){
+    this.createYearList()
+    this.createMonthDay(12)
+    this.createMonthDay(31)
+  },
+    initData(){
+          this.messageList=[{
+      message:'dsfa',
+      name:'amdog',
+      date:'2020'
+    },
+    {
+      message:'dsfa',
+      name:'amdog',
+      date:'2020'
+    },
+    {
+      message:'dsfrtfyguhijokrdtfgyuhhhierlafdsjkerudjkfwhi3ehwiufhuiwAHBFUBGWAIUHFIEHSDAJKFDHSKAJHFJKa',
+      name:'amdog',
+      date:'2020'
+    },
+    {
+      message:'dsfa',
+      name:'amdog',
+      date:'2020'
+    }]
+  }
+  createYearList(){
+    let count:number=5
+    while(count!=-1){
+      let nowYear:number=(new Date()).getFullYear()
+      this.yearList.push(nowYear-count)
+      count--
+    }
+  }
+  createMonthDay(count){
+    let endFlag=count
+    while (count!=0) {
+      endFlag==12? this.monthList.push(count):this.dayList.push(count)
+      count--
+    }
+  }
+  pageUp(){
+
+  }
+  pageDown(){
+
+  }
+  Response(index){
+    alert(index)
+  }
+  showMore(index,callResponse){
+    index==this.messageClickIndex? this.messageClickIndex=-1:this.messageClickIndex=index
+  }
+  toSelectAll(){
+    this.selectAll=!this.selectAll
+  }
 }
